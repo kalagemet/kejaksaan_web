f3c55802c95ecc82aec2bf22189231f678a28a7f
kalagemet <kalagemet@icloud.com>
2023-05-28 17:06:06 +0800

    fix lapdu v1

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://kejari-boalemo.kejaksaan.go.id';
 
     /**
      * --------------------------------------------------------------------------
@@ -36,7 +36,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
